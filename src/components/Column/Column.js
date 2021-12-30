0dcf5044bcc19598578673e84e6d3dc38f00a237
Mateusz Górski <mateusz.gorski1302@gmail.com>
2021-12-30 17:30:06 +0100

    fixRedux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,9 +1,11 @@
 import styles from './Column.module.scss';
 import Card from './../Card/Card';
 import CardForm from './../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
   const Column = props => {
 
+const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
 
 
     return (
@@ -11,13 +13,11 @@ import CardForm from './../CardForm/CardForm';
             <span className={styles.icon + ' fa fa-' + props.icon} />
             <h2 className={styles.title}>{props.title}</h2>
             <ul className={styles.cards}>
-              {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+              {cards.map(card => <Card key={card.id} title={card.title} />)}
             </ul>
              <CardForm columnId={props.id} action={props.addCard} />
-
         </article>
     );
-
 };
 
 export default Column;
